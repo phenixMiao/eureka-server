748c4b5b5dc873822eb6171e262bcb934e216b2d
phenixmfk <phenixmfk@163.com>
2018-12-25 23:08:56 +0800

    项目托管在GitHub上后的首次提交

--- src/main/java/com/phenixmfk/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/phenixmfk/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 上 GitHub啦。。。
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
